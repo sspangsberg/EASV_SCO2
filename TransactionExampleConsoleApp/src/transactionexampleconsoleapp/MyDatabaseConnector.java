50be878a73c698b00ea3bb1d8938c62432583708
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2023-02-21 20:55:32 +0100

    Local .gitignore

---
+++ TransactionExampleConsoleApp/src/transactionexampleconsoleapp/MyDatabaseConnector.java
@@ -0,0 +1,40 @@
+package sample;
+
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class MyDatabaseConnector {
+    private static final String PROP_FILE = "config/database.settings";
+    private SQLServerDataSource ds;
+
+    public MyDatabaseConnector() throws IOException {
+
+
+        Properties databaseProperties = new Properties();
+        databaseProperties.load(new FileInputStream(new File(PROP_FILE)));
+
+        String server = databaseProperties.getProperty("Server");
+        String database = databaseProperties.getProperty("Database");
+        String user = databaseProperties.getProperty("User");
+        String password = databaseProperties.getProperty("Password");
+
+        ds = new SQLServerDataSource();
+        ds.setServerName(server);
+        ds.setDatabaseName(database);
+        ds.setUser(user);
+        ds.setPassword(password);
+        ds.setTrustServerCertificate(true);
+    }
+
+    public Connection getConnection() throws SQLServerException {
+        return ds.getConnection();
+    }
+
+}
