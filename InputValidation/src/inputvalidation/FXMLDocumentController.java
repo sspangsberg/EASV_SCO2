df6987d4b0100e518f9845814cc5c0f9f6144448
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2024-02-20 00:04:18 +0100

    Update on regex, setup listeners etc.

--- InputValidation/src/inputvalidation/FXMLDocumentController.java
+++ InputValidation/src/inputvalidation/FXMLDocumentController.java
@@ -8,29 +8,64 @@ package inputvalidation;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.KeyEvent;
 
 /**
  *
  * @author spangsberg
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private TextField txtRegex;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Label lblResult;
+    @FXML
+    private TextField txtInput;
+
+    private Pattern regex = null;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        txtRegex.setOnKeyPressed(event -> {
+            regex = Pattern.compile(txtRegex.getText(), Pattern.CASE_INSENSITIVE);
+            validate(regex);
+        });
+    }
+
+    @FXML
+    private void update(ActionEvent actionEvent) {
+        // Email regex: [A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}
+        regex = Pattern.compile(txtRegex.getText(), Pattern.CASE_INSENSITIVE);
+        validate(regex);
+    }
+
+    /**
+     *
+     * @param regex
+     */
+    private void validate(Pattern regex) {
+        TextFormatter<?> formatter = new TextFormatter<>(change -> {
+            if (regex.matcher(change.getControlNewText()).matches()) {
+                lblResult.setStyle("-fx-background-color: green;");
+                return change; // allow this change to happen
+            } else {
+                lblResult.setStyle("-fx-background-color: red;");
+                return change; // prevent change
+            }
+        });
+        txtInput.setTextFormatter(formatter);
+    }
+
 }
