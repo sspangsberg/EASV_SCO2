86b79c22b80ec91036fc32d9e9c19ce4d19feb84
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2024-03-31 12:50:12 +0200

    Update password hashing project. Remove BCrypt dedicated folder and include in this project (HashPassword).

--- HashPassword/src/com/easv/Main.java
+++ HashPassword/src/com/easv/Main.java
@@ -1,35 +1,65 @@
 package com.easv;
 
+// Project imports
 import com.easv.util.crytographic.BCrypt;
+
+// Java imports
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import javax.xml.bind.DatatypeConverter;
+
 
 public class Main {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) {
 
-        String password = "123456";
+        System.out.println("MD5 (128):\t\t" + messageDigestExample("1234","MD5"));
+        System.out.println("SHA-1 (160):\t" + messageDigestExample("1234","SHA-23"));
+        System.out.println("BCrypt (192):\t" + bcryptExample("1234", true));System.out.println("SHA2-256:\t\t" + messageDigestExample("1234","SHA-256"));
+        System.out.println("SHA3-256:\t\t" + messageDigestExample("1234","SHA3-256"));
 
-        // hash + salt one-liner
-        // String hashed = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
 
-        // Randomly generated salt
-        // gensalt's log_rounds parameter determines the complexity
-        // the work factor is 2**log_rounds, and the default is 10
+    /**
+     *
+     * @param input The input to the algorithm (typically a user password)
+     * @param verbose Whether the method prints additional output information (salt, verification etc.)
+     * @return The final output (hash)
+     */
+    public static String bcryptExample(String input, boolean verbose) {
         String salt = BCrypt.gensalt(10);
 
-        // Hash a password for the first time
-        // Store this value in DB. Salt is included, so no need for separate salt column in DB
-        String hashed = BCrypt.hashpw(password, salt);
+        //hash + salt one-liner
+        String output = BCrypt.hashpw(input, salt);
 
-        // Output the salt + hash
-        System.out.println("Generated salt for (" + password + "):" + salt);
-        System.out.println("Hashed password for (" + password + "):" + hashed);
+        if (verbose) {
+            System.out.println("Salt for PW" + input + ": " + salt);
+            System.out.println("Hashed PW:" + output);
 
-        // Check that an unencrypted password matches one that has
-        // previously been hashed
-        if (BCrypt.checkpw("123456", hashed))
-            System.out.println("It matches");
-        else
-            System.out.println("It does not match");
+            if (BCrypt.checkpw(input, output))
+                System.out.println("It matches :)");
+            else
+                System.out.println("Wrong credentials");
+        }
+        return output;
+    }
+
+
+    /**
+     *
+     * @param input
+     * @param algorithm
+     * @return
+     */
+    private static String messageDigestExample(String input, String algorithm) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(algorithm);
+            md.update(input.getBytes());
+            byte[] digest = md.digest();
+
+            return DatatypeConverter.printHexBinary(digest).toUpperCase();
+        } catch (NoSuchAlgorithmException err) {
+            return err.toString();
+        }
     }
 }
